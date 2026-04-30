3361d2918651684eed85afd8e0fec658d312388b
Grady Coffey (Backup) <gjcoffey13@gmail.com>
2026-04-30 15:52:19 -0400

    Create 08_recursion_practice.cpp

---
+++ 08_recursion_practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int n) {
+    if (n == 0) return;
+    printNumbers(n - 1);
+    cout << n << " ";
+}
+
+int main() {
+    printNumbers(5);
+}
