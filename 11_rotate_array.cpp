1562a32723adc7a13b1611a4126373b4cf87f5ba
Grady Coffey (Backup) <gjcoffey13@gmail.com>
2026-04-30 15:53:41 -0400

    Create 11_rotate_array.cpp

---
+++ 11_rotate_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void rotate(int arr[], int n) {
+    int last = arr[n - 1];
+    for (int i = n - 1; i > 0; i--)
+        arr[i] = arr[i - 1];
+    arr[0] = last;
+}
+
+int main() {
+    int arr[] = {1,2,3,4};
+    rotate(arr, 4);
+    for (int x : arr) cout << x << " ";
+}
